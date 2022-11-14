d5f04cc2593c843370d3c4b04d1a3f60baa6d4dd
yudina <yudina@novotelecom.ru>
2022-11-14 14:26:26 +0700

    [Работа с массивами]: исправление недочетов

--- jsArrays/arrays.js
+++ jsArrays/arrays.js
@@ -1,40 +1,50 @@
-var originalArray = [30, 5, 9, 1, 10, 16, 25, 3, 12, 7, 6, 17, 8, 22, 13];
+(function () {
+    var originalArray = [30, 5, 9, 1, 10, 16, 25, 3, 12, 7, 6, 17, 8, 22, 13];
 
-console.log("originalArray = " + originalArray);
-console.log("descendingSortArray = " + descendingSort(originalArray));
-console.log("fiveFirstElementsArray = " + getArrayFiveFirstElements(originalArray));
-console.log("fiveLastElementsArray = " + getArrayFiveLastElements(originalArray));
-console.log("eventElementsSum = " + getSumEvenNumbers(originalArray));
-console.log("squareEvenNumbersArray = " + getArraySquareEvenNumbers(createArray(100)));
+    console.log("originalArray = " + originalArray);
+    console.log("descendingSortArray = " + sortDescending(originalArray));
+    console.log("fiveFirstElementsArray = " + getArrayFirstFirstElements(originalArray));
+    console.log("fiveLastElementsArray = " + getArrayLastFiveElements(originalArray));
+    console.log("evenElementsSum = " + getEvenNumbersSum(originalArray));
+    console.log("squareEvenNumbersArray = " + getSquaresEvenNumbersArray(createArray(100)));
 
-function descendingSort(array) {
-    return array.sort(function (e1, e2) {
-        return e2 - e1;
-    });
-}
-
-function getArrayFiveFirstElements(array) {
-    return array.slice(0, 5);
-}
-
-function getArrayFiveLastElements(array) {
-    return array.slice(array.length - 5);
-}
-
-function getSumEvenNumbers(array) {
-    return array.filter(elem => elem % 2 === 0).reduce((sum, elem) => sum + elem)
-}
-
-function createArray(number) {
-    var array = [];
-
-    for (var i = 1; i <= number; i++) {
-        array.push(i)
+    function sortDescending(array) {
+        return array.sort(function (e1, e2) {
+            return e2 - e1;
+        });
     }
 
-    return array;
-}
+    function getArrayFirstFirstElements(array) {
+        return array.slice(0, 5);
+    }
 
-function getArraySquareEvenNumbers(array) {
-    return array.filter(elem => elem % 2 === 0).map(elem => elem * elem)
-}
+    function getArrayLastFiveElements(array) {
+        return array.slice(array.length - 5);
+    }
+
+    function getEvenNumbersSum(array) {
+        return array.filter(function (e) {
+            return e % 2 === 0;
+        }).reduce(function (sum, e) {
+            return sum + e;
+        }, 0);
+    }
+
+    function createArray(number) {
+        var array = [];
+
+        for (var i = 1; i <= number; i++) {
+            array.push(i);
+        }
+
+        return array;
+    }
+
+    function getSquaresEvenNumbersArray(array) {
+        return array.filter(function (e) {
+            return e % 2 === 0;
+        }).map(function (e) {
+            return e * e;
+        })
+    }
+})()
