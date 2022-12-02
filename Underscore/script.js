8fabd7423839111332e773e2aa02128b1f0e2847
Anna-Yudina <anuta-nuty@yandex.ru>
2022-12-02 22:33:30 +0700

    [Underscore]: задача готова к сдаче!

--- Underscore/script.js
+++ Underscore/script.js
@@ -1,21 +1,19 @@
 $(function () {
-    var button = $(".button");
-    var shadow = $(".shadow");
+    var getAnswerButton = $(".get-answer");
     var answer1 = $(".answer1");
-
-    button.click(function () {
-        shadow.css({display: 'block'});
-    });
+    var answer2 = $(".answer2");
+    var answer3 = $(".answer3");
+    var answer4 = $(".answer4");
 
     var people = [
         {name: "Ivan", age: 20},
         {name: "Petr", age: 30},
-        {name: "Semen", age: 17},
+        {name: "Den", age: 17},
         {name: "Svetlana", age: 35},
         {name: "Konstantin", age: 16},
         {name: "Egor", age: 18},
         {name: "Bob", age: 32},
-        {name: "Tom", age: 40},
+        {name: "Ivan", age: 25},
         {name: "Sergey", age: 46},
         {name: "Nik", age: 38},
         {name: "Den", age: 27},
@@ -30,5 +28,31 @@ $(function () {
         .value()
         .toFixed(1);
 
-    answer1.append(peopleAverageAge);
+    var sortPeopleFrom20To30 = _.chain(people)
+        .filter(function (p) {
+            return p.age >= 20 && p.age <= 30;
+        })
+        .sortBy('age')
+        .value();
+
+    var descendingSortPeopleNameFrom20To30 = _.chain(people)
+        .filter(function (p) {
+            return p.age >= 20 && p.age <= 30;
+        })
+        .sortBy('name')
+        .pluck("name")
+        .unique()
+        .reverse()
+        .value();
+
+    var nameAndCountObject = _.chain(people)
+        .countBy("name")
+        .value();
+
+    getAnswerButton.click(function () {
+        answer1.append(peopleAverageAge);
+        answer2.append(JSON.stringify(sortPeopleFrom20To30));
+        answer3.append(descendingSortPeopleNameFrom20To30.join(", "));
+        answer4.append(JSON.stringify(nameAndCountObject));
+    });
 });
