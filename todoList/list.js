63bccc3504059ff1290938cc06c70e5167e41511
Anna-Yudina <anuta-nuty@yandex.ru>
2022-12-05 23:10:13 +0700

    [TodoList]: недочеты исправлены!

--- todoList/list.js
+++ todoList/list.js
@@ -5,17 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
     var errorMessage = document.querySelector('.error-message');
 
     addButton.addEventListener('click', function () {
-        putLine();
+        createTodoList();
     });
 
-    newText.addEventListener('keydown', (e) => {
-        if (e.code === 'Enter') {
+    newText.addEventListener('keydown', function(e) {
+        if (e.keyCode === 13) {
             e.preventDefault();
-            putLine();
+            createTodoList();
         }
     });
 
-    function putLine(){
+    function createTodoList(){
         var text = newText.value.trim();
 
         errorMessage.style.display = 'none';
@@ -32,24 +32,26 @@ document.addEventListener('DOMContentLoaded', function () {
         function setEditMode() {
             item.innerHTML = "<input class='edit-text' type='text' maxlength='85'>" +
                 "<div class='buttons'>" +
-                "<button class='ok-button' type='button'>&#10004;</button>" +
-                "<button class='cancel-button' type='button'>&#8634;</button></div>";
+                "<button class='ok-button' type='button' title='ok'>&#10004;</button>" +
+                "<button class='cancel-button' type='button' title='отмена'>&#8634;</button></div>" +
+                "<div style = 'display: none' class='edit-error-message'>Текст не должен быть пустым!</div>";
 
             item.querySelector('.edit-text').value = text;
 
             item.querySelector('.ok-button').addEventListener('click', function () {
                 var editTextElement = item.querySelector('.edit-text');
-                var editText = editTextElement.value;
-                errorMessage.style.display = 'none';
+                var editText = editTextElement.value.trim();
+                var editErrorMessage = document.querySelector('.edit-error-message');
+                editErrorMessage.style.display = 'none';
                 newText.classList.remove('red-border');
 
                 if (editText.length === 0) {
-                    errorMessage.style.display = 'block';
+                    editErrorMessage.style.display = 'block';
                     editTextElement.classList.add('red-border');
                     return;
                 }
 
-                text = item.querySelector('.edit-text').value;
+                text = editText;
                 setViewMode();
             });
 
@@ -61,12 +63,12 @@ document.addEventListener('DOMContentLoaded', function () {
         function setViewMode() {
             item.innerHTML = "<div class='todo-text'></div>" +
                 "<div class='buttons'>" +
-                "<button class='delete-button' type='button'>&#10006;</button> " +
-                "<button class='edit-button' type='button'>&#9998;</button></div>";
+                "<button class='delete-button' type='button' title='удалить'>&#10006;</button> " +
+                "<button class='edit-button' type='button' title='редактировать'>&#9998;</button></div>";
 
             item.querySelector('.todo-text').textContent = text;
 
-            item.querySelector(".delete-button").addEventListener('click', function () {
+            item.querySelector('.delete-button').addEventListener('click', function () {
                 errorMessage.style.display = 'none';
                 newText.classList.remove('red-border');
                 item.remove();
