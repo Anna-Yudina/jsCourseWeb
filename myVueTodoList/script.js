9289a8732b7b3ddd4992c523148c69cd26a789ac
Anna-Yudina <anuta-nuty@yandex.ru>
2022-12-21 22:44:51 +0700

    [Todo List Vue]: задача готова к проверке

---
+++ myVueTodoList/script.js
@@ -0,0 +1,96 @@
+Vue.component("todo-item", {
+    template: "#todo-item-template",
+
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },
+
+    data: function () {
+        return {
+            isEditing: false,
+            editingText: "",
+            isInvalid: false
+        }
+    },
+
+    methods: {
+        editItem: function () {
+            this.isInvalid = false;
+            this.editingText = this.item.text;
+            this.isEditing = true;
+        },
+
+        saveItem: function () {
+            this.isInvalid = false;
+            var newValue = this.editingText;
+
+            if (newValue.length === 0) {
+                this.isInvalid = true;
+                return;
+            }
+
+            this.$emit("save-item", this.item, this.editingText);
+            this.isEditing = false;
+        },
+    },
+
+    watch: {
+        inputText: function (newValue) {
+            if (newValue.length > 0) {
+                this.isInvalid = false;
+            }
+        }
+    }
+});
+
+var vm = new Vue({
+    el: "#container",
+
+    data: {
+        isInputTextInvalid: false,
+        items: [],
+        inputText: "",
+        currentItemId: 1
+    },
+
+    methods: {
+        add: function () {
+            this.isInputTextInvalid = false;
+
+            if (this.inputText.length === 0) {
+                this.isInputTextInvalid = true;
+                return;
+            }
+
+            this.items.push({
+                id: this.currentItemId,
+                text: this.inputText
+
+            })
+
+            this.inputText = "";
+            this.currentItemId++;
+        },
+
+        deleteItem: function (item) {
+            this.items = this.items.filter(function (x) {
+                return x !== item;
+            })
+        },
+
+        saveItem: function (item, newText) {
+            item.text = newText;
+        },
+    },
+
+    watch: {
+        inputText: function (newValue) {
+            if (newValue.length > 0) {
+                this.isInputTextInvalid = false;
+            }
+        }
+    }
+});
