67fa048df598fa04db43ea8095654a26d9686a4c
Anna-Yudina <anuta-nuty@yandex.ru>
2022-12-27 22:00:43 +0700

    [Underscore]: исправление недочетов

--- Underscore/script.js
+++ Underscore/script.js
@@ -1,9 +1,9 @@
 $(function () {
     var getAnswerButton = $(".get-answer");
-    var answer1 = $(".answer1");
-    var answer2 = $(".answer2");
-    var answer3 = $(".answer3");
-    var answer4 = $(".answer4");
+    var answer1 = $(".answer-1");
+    var answer2 = $(".answer-2");
+    var answer3 = $(".answer-3");
+    var answer4 = $(".answer-4");
 
     var people = [
         {name: "Ivan", age: 20},
@@ -17,42 +17,40 @@ $(function () {
         {name: "Sergey", age: 46},
         {name: "Nik", age: 38},
         {name: "Den", age: 27},
-        {name: "Max", age: 34}
+        {name: "Ivan", age: 34}
     ];
 
-    var peopleAverageAge = _.chain(people)
+    var peopleAverageAge = (_.chain(people)
         .pluck("age")
-        .reduce(function (memo, item) {
-            return memo + item / _.size(people)
+        .reduce(function (accumulatedAge, age) {
+            return accumulatedAge + age
         }, 0)
-        .value()
-        .toFixed(1);
+        .value()) / _.size(people)
 
-    var sortPeopleFrom20To30 = _.chain(people)
+
+    var from20To30SortedPeople = _.chain(people)
         .filter(function (p) {
             return p.age >= 20 && p.age <= 30;
         })
-        .sortBy('age')
+        .sortBy("age")
         .value();
 
-    var descendingSortPeopleNameFrom20To30 = _.chain(people)
+    var from20To30DescendingSortedPeopleNames = _.chain(people)
         .filter(function (p) {
             return p.age >= 20 && p.age <= 30;
         })
-        .sortBy('name')
         .pluck("name")
-        .unique()
+        .uniq()
+        .sortBy()
         .reverse()
         .value();
 
-    var nameAndCountObject = _.chain(people)
-        .countBy("name")
-        .value();
+    var countNamesObject = _.countBy(people, "name");
 
     getAnswerButton.click(function () {
-        answer1.append(peopleAverageAge);
-        answer2.append(JSON.stringify(sortPeopleFrom20To30));
-        answer3.append(descendingSortPeopleNameFrom20To30.join(", "));
-        answer4.append(JSON.stringify(nameAndCountObject));
+        answer1.append(peopleAverageAge.toFixed(1));
+        answer2.append(JSON.stringify(from20To30SortedPeople));
+        answer3.append(from20To30DescendingSortedPeopleNames.join(", "));
+        answer4.append(JSON.stringify(countNamesObject));
     });
 });
